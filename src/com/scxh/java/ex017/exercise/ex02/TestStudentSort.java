5fb1530cd724c84d1113051952246491aac5b384
viktor.zhou <2915858910@qq.com>
2015-05-15 15:05:46 +0800

    容器总结

--- src/com/scxh/java/ex017/exercise/ex02/TestStudentSort.java
+++ src/com/scxh/java/ex017/exercise/ex02/TestStudentSort.java
@@ -7,144 +7,148 @@ import java.util.Iterator;
 import java.util.List;
 
 public class TestStudentSort {
+	public static void main(String[] args) {
+		 sortComparable();
+//		sortComparator();
+
+	}
 
 	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-//		sortComparable();
-		sortComparator();
-		
-	}
-	public static void sortComparable(){
-		Student stuA = new Student("小明",30);
-		Student stuB = new Student("张三",42);
-		Student stuC = new Student("王二",29);
-		Student stuE = new Student("李四",29);
-		Student stuD = new Student("大彬",18);
-		
-		
+	 * 对容器中元素进行排序 ,使用Comparable
+	 */ 
+	public static void sortComparable() {
 		ArrayList<Student> list = new ArrayList<Student>();
-		list.add(stuA);
-		list.add(stuB);
-		list.add(stuC);
-		list.add(stuD);
-		list.add(stuE);
 		
-		
-		Iterator<Student> iterator = list.iterator();
-		while(iterator.hasNext()){
-			Student stu = iterator.next();
-			String name = stu.getName();
-			int number = stu.getNumber();
-			System.out.println("姓名 :"+name+ " 学号 :"+number);
-		}
+		addStudentsToList(list);
 
-		
+		showStudentsList(list);
+
 		System.out.println("=========对容器中元素进行排序=使用Comparable===========");
-		
+
 		Collections.sort(list);
-		
-		
-		iterator = list.iterator();
-		while(iterator.hasNext()){
-			Student stu = iterator.next();
-			String name = stu.getName();
-			int number = stu.getNumber();
-			System.out.println("姓名 :"+name+ " 学号 :"+number);
-		}
+
+		showStudentsList(list);
 	}
-	
-	
-	public static void sortComparator(){
-		Student stuA = new Student("小明",30);
-		Student stuB = new Student("张三",42);
-		Student stuC = new Student("王二",29);
-		Student stuE = new Student("李四",29);
-		Student stuD = new Student("大彬",18);
-		
-		
+
+	/**
+	 * 对容器中元素进行排序,使用Comparator
+	 */
+	public static void sortComparator() {
 		ArrayList<Student> list = new ArrayList<Student>();
-		list.add(stuA);
-		list.add(stuB);
-		list.add(stuC);
-		list.add(stuD);
-		list.add(stuE);
 		
-		
-		showList(list);
+		addStudentsToList(list);
+
+		showStudentsList(list);
 
 		System.out.println("=========对容器中元素进行排序=使用Comparator===========");
-		Collections.sort(list,new MyComparatorTwo());
+
 		
-		showList(list);
+		//《《《《《《《《 必须撑握》》》》
+		Collections.sort(list, new MyComparatorTwo());   
+		
+//		sortLists(list);
+		
+		showStudentsList(list);
 	}
+
 	/**
-	 * 遍历容器 
+	 * 添加学生对象到容器，返回存储学生对象容器对象
+	 * 
+	 * @return
+	 */
+	public static void addStudentsToList(ArrayList<Student> list) {
+		Student stuA = new Student("小明", 30);
+		Student stuB = new Student("张三", 42);
+		Student stuC = new Student("王二", 29);
+		Student stuE = new Student("李四", 29);
+		Student stuD = new Student("大彬", 18);
+
+		list.add(stuA);
+		list.add(stuB);
+		list.add(stuC);
+		list.add(stuD);
+		list.add(stuE);
+
+	}
+
+	/**
+	 * 遍历容器显示学生姓名和学号
+	 * 
 	 * @param list
 	 */
-	public static void showList(ArrayList list){
+	public static void showStudentsList(ArrayList<Student> list) {
 		Iterator<Student> iterator = list.iterator();
-		while(iterator.hasNext()){
+		
+		while (iterator.hasNext()) {
 			Student stu = iterator.next();
 			String name = stu.getName();
 			int number = stu.getNumber();
-			System.out.println("姓名 :"+name+ " 学号 :"+number);
+			System.out.println("姓名 :" + name + " 学号 :" + number);
 		}
 	}
-	/**
-	 * 排序实现方法汇总
-	 * @param list
-	 * @return
-	 */
-	public static ArrayList<Student> sortList(ArrayList<Student> list){
-		//================方法一 ===匿名内部类================
-		Collections.sort(list,new Comparator<Student>() {
+
+	public static void sortLists(ArrayList<Student> list) {
+		// ================方法一 ===匿名内部类================
+		Collections.sort(list, new Comparator<Student>() {
 
 			public int compare(Student o1, Student o2) {
 				int num1 = o1.getNumber();
 				int num2 = o2.getNumber();
-				
-				return num1 > num2 ? 1:-1;
+
+				return num1 > num2 ? 1 : -1;
 			}
 		});
-		
-		
-		//================方法二 ===================
+	}
+	
+	/**
+	 * 排序实现方法汇总
+	 * 
+	 * @param list
+	 * @return
+	 */
+	public static ArrayList<Student> sortList(ArrayList<Student> list) {
+		// ================方法一 ===匿名内部类================
+		Collections.sort(list, new Comparator<Student>() {
+
+			public int compare(Student o1, Student o2) {
+				int num1 = o1.getNumber();
+				int num2 = o2.getNumber();
+
+				return num1 > num2 ? 1 : -1;
+			}
+		});
+
+		// ================方法二 ===================
 		Comparator<Student> comaprator = new Comparator<Student>() {
 
 			public int compare(Student o1, Student o2) {
 				int num1 = o1.getNumber();
 				int num2 = o2.getNumber();
-				
-				return num1 > num2 ? 1:-1;
+
+				return num1 > num2 ? 1 : -1;
 			}
 		};
-		
-		Collections.sort(list,comaprator);
-		
-		
-		//================= 方法三  ==内部类 MyComparator====================
-		Collections.sort(list,new TestStudentSort().new MyComparator());
-		
-		
-		
-		//================= 方法四 ======================
-		Collections.sort(list,new MyComparatorTwo());
-		
-		
+
+		Collections.sort(list, comaprator);
+
+		// ================= 方法三 ==内部类 MyComparator====================
+		Collections.sort(list, new TestStudentSort().new MyComparator());
+
+		// ================= 方法四 ======================
+		Collections.sort(list, new MyComparatorTwo());
+
 		return list;
 	}
-	
-	class MyComparator implements Comparator<Student>{
+
+	class MyComparator implements Comparator<Student> {
 
 		public int compare(Student o1, Student o2) {
 			int num1 = o1.getNumber();
 			int num2 = o2.getNumber();
-			
-			return num1 > num2 ? 1:-1;
+
+			return num1 > num2 ? 1 : -1;
 		}
-		
+
 	}
 
 }
